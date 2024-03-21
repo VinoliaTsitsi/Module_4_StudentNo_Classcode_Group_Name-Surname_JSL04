13d51c0b42d10a2aa814414b5f29c7f358810199
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-03-21 16:20:27 +0200

    added changes to the code

--- index.js
+++ index.js
@@ -17,36 +17,19 @@ cansLeft < 5 ? console.log("Time to restock!"): console.log("We're stocked");
 
 // #### Challenge 3: Workout Intensity
 
-let heartRate = 95; // Current heart rate in bpm
+let heartRate = 101; // Current heart rate in bpm
 
 heartRate < 100? console.log('Boost needed!') : console.log('Energy levels are high!');
 
 
 // #### Challenge 4: Temperature Suitability
-// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
-// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+
+currentTemp === 4 || currentTemp <= 5 ? console.log('Chilled to perfection!'): console.log('Needs a cooler!')
+
+// #### Challenge 5: Late Night Coding Session 
+let currentHour = 5; 
+currentHour >= 7 && currentHour < 24 ? console.log('Unleash the beast!'): console.log('Better stick to water')
 
 
-// #### Challenge 5: Late Night Coding Session
-// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
-// otherwise, print "Better stick to water."
-
-// Define the current hour in 24-hour format
-let currentHour = 22; 
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
-
-
-
-
-
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
